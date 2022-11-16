28a503c395322c1b8e3e490e4ed870febd0b16a1
Alwin den Herder <42200913+Alwindh@users.noreply.github.com>
2022-11-16 18:25:17 +0100

    now displays scores

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useCookies } from "react-cookie";
+import { useEffect, useState } from "react";
+import { Container } from "@mui/material";
+
+function App() {
+	const [cookies, setCookie, removeCookie] = useCookies(["seven-card-game"]);
+	const [players, setPlayers] = useState();
+	const [dealer, setDealer] = useState();
+	const [maxPoints, setMaxPoints] = useState();
+	const [turns, setTurns] = useState();
+	const [currentScores, setCurrentScores] = useState();
+
+	useEffect(() => {
+		setPlayers(["Alwin", "Charla", "Phil", "Janet"]);
+		setDealer(2);
+		setMaxPoints(500);
+		setTurns({
+			1: { Charla: 50, Alwin: 100, Phil: 0, Janet: 5, jokercard: "4c" },
+			2: { Charla: 50, Alwin: 100, Phil: 0, Janet: 5, jokercard: "4c" },
+		});
+	}, []);
+
+	useEffect(() => {
+		if (players && turns) {
+			let currentScores = {};
+			for (const [turnKey, turnValue] of Object.entries(turns)) {
+				for (const [nameKey, nameValue] of Object.entries(turnValue)) {
+					if (nameKey in currentScores && players.includes(nameKey)) {
+						currentScores[nameKey] += nameValue;
+					} else if (players.includes(nameKey)) {
+						currentScores[nameKey] = nameValue;
+					}
+				}
+			}
+			setCurrentScores(currentScores);
+			console.log(currentScores);
+		}
+	}, [players, turns]);
+
+	return (
+		<Container>
+			{currentScores &&
+				players.map((playerElement) => {
+					return currentScores[playerElement];
+				})}
+		</Container>
+	);
+}
+
+export default App;
